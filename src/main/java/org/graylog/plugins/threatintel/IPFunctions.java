52152af17dc1da6ad330670697fec44a3d6ed545
Dennis Oelkers <dennis@graylog.com>
2017-08-16 13:22:01 +0200

    Retain annotations at runtime, make binding annotations.

--- src/main/java/org/graylog/plugins/threatintel/IPFunctions.java
+++ src/main/java/org/graylog/plugins/threatintel/IPFunctions.java
@@ -1,4 +1,12 @@
 package org.graylog.plugins.threatintel;
 
+import com.google.inject.BindingAnnotation;
+
+import java.lang.annotation.Retention;
+
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Retention(RUNTIME)
+@BindingAnnotation
 public @interface IPFunctions {
 }
