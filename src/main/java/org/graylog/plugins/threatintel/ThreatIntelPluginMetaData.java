2ca589aacc7db504f7f81f448c4bdec35ac50ce3
Bernd Ahlers <bernd@graylog.com>
2017-09-26 12:08:34 +0200

    Fix plugin author formatting

--- src/main/java/org/graylog/plugins/threatintel/ThreatIntelPluginMetaData.java
+++ src/main/java/org/graylog/plugins/threatintel/ThreatIntelPluginMetaData.java
@@ -23,7 +23,7 @@ public class ThreatIntelPluginMetaData implements PluginMetaData {
 
     @Override
     public String getAuthor() {
-        return "Graylog, Inc";
+        return "Graylog, Inc.";
     }
 
     @Override
