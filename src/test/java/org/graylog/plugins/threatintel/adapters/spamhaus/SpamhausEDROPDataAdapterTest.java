cae7df6a776a5222e9fb2fa94a77f1166dd422e8
Dennis Oelkers <dennis@graylog.com>
2017-10-04 17:50:24 +0200

    Adding test case for partial list update.

--- src/test/java/org/graylog/plugins/threatintel/adapters/spamhaus/SpamhausEDROPDataAdapterTest.java
+++ src/test/java/org/graylog/plugins/threatintel/adapters/spamhaus/SpamhausEDROPDataAdapterTest.java
@@ -6,6 +6,7 @@ import org.graylog2.lookup.adapters.dsvhttp.HTTPFileRetriever;
 import org.graylog2.plugin.lookup.LookupCachePurge;
 import org.graylog2.plugin.lookup.LookupDataAdapterConfiguration;
 import org.graylog2.plugin.lookup.LookupResult;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.mockito.Mock;
@@ -20,6 +21,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.AbstractMap;
+import java.util.Map;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -34,6 +36,8 @@ public class SpamhausEDROPDataAdapterTest {
     private final String dropSnapshot = readResourcesFile("drop.txt-snapshot-201709291400");
     private final String edropSnapshot = readResourcesFile("edrop.txt-snapshot-201709291400");
 
+    private SpamhausEDROPDataAdapter adapter;
+
     public SpamhausEDROPDataAdapterTest() throws IOException, URISyntaxException {
     }
 
@@ -43,15 +47,18 @@ public class SpamhausEDROPDataAdapterTest {
         return new String(Files.readAllBytes(torExitNodeListPath), StandardCharsets.UTF_8);
     }
 
-    @Test
-    public void tableStateShouldRetrieveListsSuccessfully() throws Exception {
-        final SpamhausEDROPDataAdapter adapter = new SpamhausEDROPDataAdapter("foobar",
+    @Before
+    public void setUp() throws Exception {
+        this.adapter = new SpamhausEDROPDataAdapter("foobar",
                 "foobar",
                 mock(LookupDataAdapterConfiguration.class),
                 new DSVHTTPDataAdapter.Descriptor(),
                 mock(MetricRegistry.class),
                 httpFileRetriever);
+    }
 
+    @Test
+    public void tableStateShouldRetrieveListsSuccessfully() throws Exception {
         when(httpFileRetriever.fetchFileIfNotModified("https://www.spamhaus.org/drop/drop.txt")).thenReturn(Optional.of(dropSnapshot));
         when(httpFileRetriever.fetchFileIfNotModified("https://www.spamhaus.org/drop/edrop.txt")).thenReturn(Optional.of(edropSnapshot));
         adapter.doStart();
@@ -61,13 +68,6 @@ public class SpamhausEDROPDataAdapterTest {
 
     @Test
     public void tableStateShouldStartupIfServiceMalfunctions() throws Exception {
-        final SpamhausEDROPDataAdapter adapter = new SpamhausEDROPDataAdapter("foobar",
-                "foobar",
-                mock(LookupDataAdapterConfiguration.class),
-                new DSVHTTPDataAdapter.Descriptor(),
-                mock(MetricRegistry.class),
-                httpFileRetriever);
-
         when(httpFileRetriever.fetchFileIfNotModified("https://www.spamhaus.org/drop/drop.txt")).thenReturn(Optional.ofNullable(null));
         when(httpFileRetriever.fetchFileIfNotModified("https://www.spamhaus.org/drop/edrop.txt")).thenReturn(Optional.ofNullable(null));
 
@@ -80,13 +80,6 @@ public class SpamhausEDROPDataAdapterTest {
 
     @Test
     public void tableStateShouldRetainStateIfServiceMalfunctions() throws Exception {
-        final SpamhausEDROPDataAdapter adapter = new SpamhausEDROPDataAdapter("foobar",
-                "foobar",
-                mock(LookupDataAdapterConfiguration.class),
-                new DSVHTTPDataAdapter.Descriptor(),
-                mock(MetricRegistry.class),
-                httpFileRetriever);
-
         when(httpFileRetriever.fetchFileIfNotModified("https://www.spamhaus.org/drop/drop.txt")).thenReturn(Optional.of(dropSnapshot));
         when(httpFileRetriever.fetchFileIfNotModified("https://www.spamhaus.org/drop/edrop.txt")).thenReturn(Optional.of(edropSnapshot));
 
@@ -103,34 +96,74 @@ public class SpamhausEDROPDataAdapterTest {
         verifyAdapterFunctionality(adapter);
     }
 
+    @Test
+    public void tableStateShouldHaveProperStateWhenOneListIsNotUpdated() throws Exception {
+        when(httpFileRetriever.fetchFileIfNotModified("https://www.spamhaus.org/drop/drop.txt")).thenReturn(Optional.of("192.168.1.0/24 ; SBL0815\n"));
+        when(httpFileRetriever.fetchFileIfNotModified("https://www.spamhaus.org/drop/edrop.txt")).thenReturn(Optional.of("10.1.0.0/16 ; SBL2342\n"));
+
+        adapter.doStart();
+
+        final LookupResult dropLookupResult = adapter.doGet("192.168.1.1");
+        assertLookupResultHasMultiValue(dropLookupResult, entry("sbl_id", "SBL0815"), entry("subnet", "192.168.1.0/24"));
+
+        final LookupResult edropLookupResult = adapter.doGet("10.1.0.1");
+        assertLookupResultHasMultiValue(edropLookupResult, entry("sbl_id", "SBL2342"), entry("subnet", "10.1.0.0/16"));
+
+        when(httpFileRetriever.fetchFileIfNotModified("https://www.spamhaus.org/drop/drop.txt")).thenReturn(Optional.of("172.16.0.0/16 ; SBL1919\n"));
+        when(httpFileRetriever.fetchFileIfNotModified("https://www.spamhaus.org/drop/edrop.txt")).thenReturn(Optional.ofNullable(null));
+
+        final LookupCachePurge lookupCachePurge = mock(LookupCachePurge.class);
+        adapter.doRefresh(lookupCachePurge);
+
+        verify(lookupCachePurge, times(1)).purgeAll();
+        verify(lookupCachePurge, never()).purgeKey(any());
+
+        final LookupResult negativeDropLookupResult = adapter.doGet("192.168.1.1");
+        assertNegativeLookupResult(negativeDropLookupResult);
+
+        final LookupResult dropLookupResult2 = adapter.doGet("172.16.0.1");
+        assertLookupResultHasMultiValue(dropLookupResult2, entry("sbl_id", "SBL1919"), entry("subnet", "172.16.0.0/16"));
+
+        final LookupResult edropLookupResult2 = adapter.doGet("10.1.0.1");
+        assertLookupResultHasMultiValue(edropLookupResult2, entry("sbl_id", "SBL2342"), entry("subnet", "10.1.0.0/16"));
+    }
+
     private void verifyAdapterFunctionality(SpamhausEDROPDataAdapter adapter) {
         final LookupResult dropLookupResult = adapter.doGet("209.66.128.1");
-        assertThat(dropLookupResult).isNotNull();
-        assertThat(dropLookupResult.isEmpty()).isFalse();
-        assertThat(dropLookupResult.singleValue()).isNotNull();
-        assertThat((Boolean) dropLookupResult.singleValue()).isTrue();
-        assertThat(dropLookupResult.multiValue()).containsExactly(
-                new AbstractMap.SimpleEntry<Object, Object>("sbl_id", "SBL180438"),
-                new AbstractMap.SimpleEntry<Object, Object>("subnet", "209.66.128.0/19")
+        assertLookupResultHasMultiValue(dropLookupResult,
+                entry("sbl_id", "SBL180438"),
+                entry("subnet", "209.66.128.0/19")
         );
 
         final LookupResult edropLookupResult = adapter.doGet("221.132.192.42");
-        assertThat(edropLookupResult).isNotNull();
-        assertThat(edropLookupResult.isEmpty()).isFalse();
-        assertThat(edropLookupResult.singleValue()).isNotNull();
-        assertThat((Boolean) edropLookupResult.singleValue()).isTrue();
-        assertThat(edropLookupResult.multiValue()).containsExactly(
-                new AbstractMap.SimpleEntry<Object, Object>("sbl_id", "SBL233662"),
-                new AbstractMap.SimpleEntry<Object, Object>("subnet", "221.132.192.0/18")
+        assertLookupResultHasMultiValue(edropLookupResult,
+                entry("sbl_id", "SBL233662"),
+                entry("subnet", "221.132.192.0/18")
         );
 
         final LookupResult negativeLookup = adapter.doGet("1.2.3.4");
+        assertNegativeLookupResult(negativeLookup);
+    }
+    
+    private void assertLookupResultHasMultiValue(LookupResult lookupResult, Map.Entry<Object, Object>... entries) {
+        assertThat(lookupResult).isNotNull();
+        assertThat(lookupResult.isEmpty()).isFalse();
+        assertThat(lookupResult.singleValue()).isNotNull();
+        assertThat((Boolean) lookupResult.singleValue()).isTrue();
+        assertThat(lookupResult.multiValue()).containsExactly(entries);
+    }
+
+    private void assertNegativeLookupResult(LookupResult negativeLookup) {
         assertThat(negativeLookup).isNotNull();
         assertThat(negativeLookup.isEmpty()).isFalse();
         assertThat(negativeLookup.singleValue()).isNotNull();
         assertThat((Boolean) negativeLookup.singleValue()).isFalse();
         assertThat(negativeLookup.multiValue()).containsExactly(
-                new AbstractMap.SimpleEntry<Object, Object>("value", false)
+                entry("value", false)
         );
+    }
+
+    private Map.Entry<Object, Object> entry(String key, Object value) {
+        return new AbstractMap.SimpleEntry<Object, Object>(key, value);
     }
 }
