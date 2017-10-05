42bad127dd8fc4853429d5132afb30240caeea2d
Dennis Oelkers <dennis@graylog.com>
2017-10-05 12:25:05 +0200

    Adapting constructor usage in test to changed signature.

--- src/test/java/org/graylog/plugins/threatintel/adapters/spamhaus/SpamhausEDROPDataAdapterTest.java
+++ src/test/java/org/graylog/plugins/threatintel/adapters/spamhaus/SpamhausEDROPDataAdapterTest.java
@@ -51,7 +51,6 @@ public class SpamhausEDROPDataAdapterTest {
     public void setUp() throws Exception {
         this.adapter = new SpamhausEDROPDataAdapter("foobar",
                 "foobar",
-                mock(LookupDataAdapterConfiguration.class),
                 new DSVHTTPDataAdapter.Descriptor(),
                 mock(MetricRegistry.class),
                 httpFileRetriever);
