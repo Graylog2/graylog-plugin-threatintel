1c8ec5e2ffebc11e566006fbc40fb3d0aaf7f5c7
Bernd Ahlers <bernd@users.noreply.github.com>
2017-12-19 13:00:06 +0100

    Always use ARIN registry for now and remove registry form config forms (#78)
    Closes #76
    
    (cherry picked from commit 3d876f59681d583e61865e26fb8ec3c0b19275be)

--- src/web/components/adapters/whois/WhoisAdapterFieldSet.jsx
+++ src/web/components/adapters/whois/WhoisAdapterFieldSet.jsx
@@ -1,22 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import lodash from 'lodash';
 
 import { Input } from 'components/bootstrap';
-import { Select } from 'components/common';
-
-const WHOIS_REGISTRIES = [
-  { value: 'AFRINIC', label: 'AFRINIC (Africa)' },
-  { value: 'APNIC', label: 'APNIC (Asia/Pacific)' },
-  { value: 'ARIN', label: 'ARIN (North America)' },
-  { value: 'LACNIC', label: 'LACNIC (South America)' },
-  { value: 'RIPENCC', label: 'RIPENCC (Europe)' },
-];
 
 const WhoisAdapterFieldSet = React.createClass({
   propTypes: {
     config: PropTypes.shape({
-      registry: PropTypes.string.isRequired,
       connect_timeout: PropTypes.number.isRequired,
       read_timeout: PropTypes.number.isRequired,
     }).isRequired,
@@ -25,34 +14,11 @@ const WhoisAdapterFieldSet = React.createClass({
     validationState: PropTypes.func.isRequired,
   },
 
-  handleSelect(fieldName) {
-    return (selectedIndicator) => {
-      const config = lodash.cloneDeep(this.props.config);
-      config[fieldName] = selectedIndicator;
-      this.props.updateConfig(config);
-    };
-  },
-
   render() {
     const { config } = this.props;
 
     return (
       <fieldset>
-        <Input id="registry"
-               label="Registry"
-               required
-               onChange={this.props.handleFormEvent}
-               help={this.props.validationMessage('registry', 'The registry used for the initial lookup, should be the closest to your location.')}
-               bsStyle={this.props.validationState('registry')}
-               labelClassName="col-sm-3"
-               wrapperClassName="col-sm-9">
-          <Select placeholder="Select registry"
-                  clearable={false}
-                  options={WHOIS_REGISTRIES}
-                  matchProp="value"
-                  onChange={this.handleSelect('registry')}
-                  value={config.registry} />
-        </Input>
         <Input type="number"
                id="connect_timeout"
                name="connect_timeout"
