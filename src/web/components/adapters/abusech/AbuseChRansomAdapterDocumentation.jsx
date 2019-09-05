fe452428cf9ec42e15e3d41451a298f5fa5a1a9a
Kyle Knight <kyle@graylog.com>
2019-09-05 10:25:12 -0500

    AbuseChRansomAdapterDocumentation

--- src/web/components/adapters/abusech/AbuseChRansomAdapterDocumentation.jsx
+++ src/web/components/adapters/abusech/AbuseChRansomAdapterDocumentation.jsx
@@ -1,11 +1,11 @@
-/* eslint-disable react/no-unescaped-entities */
 import React from 'react';
-import { Alert } from 'react-bootstrap';
 
-class AbuseChRansomAdapterDocumentation extends React.Component {
-  render() {
-    return (<div>
-      <p>The <a href="https://ransomwaretracker.abuse.ch/blocklist/" target="_blank">abuse.ch ransomware tracker</a> offers various types of blocklists that allows you to block Ransomware botnet C&C traffic.</p>
+import { Alert } from 'components/graylog';
+
+const AbuseChRansomAdapterDocumentation = () => {
+  return (
+    <div>
+      <p>The <a href="https://ransomwaretracker.abuse.ch/blocklist/" target="_blank" rel="noopener noreferrer">abuse.ch ransomware tracker</a> offers various types of blocklists that allows you to block Ransomware botnet C&amp;C traffic.</p>
 
       <Alert style={{ marginBottom: 10 }} bsStyle="info">
         <h4 style={{ marginBottom: 10 }}>Limitations</h4>
@@ -13,8 +13,8 @@ class AbuseChRansomAdapterDocumentation extends React.Component {
         <p>For support of individual blocklists, please visit our support channels.</p>
       </Alert>
 
-    </div>);
-  }
-}
+    </div>
+  );
+};
 
 export default AbuseChRansomAdapterDocumentation;
