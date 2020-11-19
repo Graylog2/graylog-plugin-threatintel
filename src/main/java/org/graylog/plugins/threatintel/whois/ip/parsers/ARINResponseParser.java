97b4d76c3bf3f4761d03bfc1158108d4a2e38cd5
Rob Curtis <rob.curtis@graylog.com>
2020-11-19 10:15:34 -0600

    Update WHOIS lookup and ARIN parser to better handle cases with multiple results.

--- src/main/java/org/graylog/plugins/threatintel/whois/ip/parsers/ARINResponseParser.java
+++ src/main/java/org/graylog/plugins/threatintel/whois/ip/parsers/ARINResponseParser.java
@@ -18,13 +18,27 @@ package org.graylog.plugins.threatintel.whois.ip.parsers;
 
 public class ARINResponseParser extends WhoisParser {
 
+    private NetworkType prevNetworkType = null;
+    private NetworkType currNetworkType = null;
+
     @Override
     public void readLine(String line) {
         if (line.startsWith("#") || line.isEmpty()) {
             return;
         }
 
-        if(line.startsWith("Organization:") && this.organization == null) {
+        // In some cases, ARIN may have multiple results with different NetType values.  When that happens,
+        //  we want to use the data from the entry with the data closest to the customer actually using the IP.
+        if (line.startsWith("NetType:")) {
+            prevNetworkType = currNetworkType;
+            currNetworkType = NetworkType.getEnum(lineValue(line));
+            if (null != currNetworkType && currNetworkType.isMoreSpecificThan(prevNetworkType)) {
+                this.organization = null;
+                this.countryCode = null;
+            }
+        }
+
+        if((line.startsWith("Organization:") || line.startsWith("Customer:")) && this.organization == null) {
             this.organization = lineValue(line);
         }
 
@@ -38,4 +52,41 @@ public class ARINResponseParser extends WhoisParser {
         }
     }
 
+    @Override
+    public String buildQueryForIp(String ip) {
+        // This query ensures that we get all of the records when there are multiple results rather than just a list of
+        //  record summaries without details.
+        return "n + " + ip;
+    }
+
+    private enum NetworkType {
+        // Network types are defined in ARIN's documentation: https://www.arin.net/resources/registry/whois/#network
+        // Arranged in order of decreasing preference.  Do not reorder unless preference order changes
+        REASSIGNED("Reassigned"),
+        DIRECT_ASSIGNMENT("Direct Assignment"),
+        REALLOCATED("Reallocated"),
+        DIRECT_ALLOCATION("Direct Allocation");
+
+        private String displayName;
+
+        NetworkType(String displayName) { this.displayName = displayName; }
+
+        String displayName() { return displayName; }
+
+        boolean isMoreSpecificThan(NetworkType netType) {
+            if (null == netType) {
+                return true;
+            }
+            return (this.ordinal() < netType.ordinal());
+        }
+
+        static NetworkType getEnum(String value) {
+            for (NetworkType v : values()) {
+                if (value.equalsIgnoreCase(v.displayName())) {
+                    return v;
+                }
+            }
+            return null;
+        }
+    }
 }
