ea12d3f1ee1c93031405856176c4e8495a635e69
Rob Curtis <6466251+waab76@users.noreply.github.com>
2020-12-10 09:50:49 -0500

    Deprecate Ransomware Data Adapter (#183)

--- src/web/components/adapters/abusech/AbuseChRansomAdapterDocumentation.jsx
+++ src/web/components/adapters/abusech/AbuseChRansomAdapterDocumentation.jsx
@@ -21,14 +21,10 @@ import { Alert } from 'components/graylog';
 const AbuseChRansomAdapterDocumentation = () => {
   return (
     <div>
-      <p>The <a href="https://ransomwaretracker.abuse.ch/blocklist/" target="_blank" rel="noopener noreferrer">abuse.ch ransomware tracker</a> offers various types of blocklists that allows you to block Ransomware botnet C&amp;C traffic.</p>
-
-      <Alert style={{ marginBottom: 10 }} bsStyle="info">
-        <h4 style={{ marginBottom: 10 }}>Limitations</h4>
-        <p>Currently only the combined blocklists are supported.</p>
-        <p>For support of individual blocklists, please visit our support channels.</p>
+      <Alert style={{ marginBottom: 10 }} bsStyle="warning">
+        <h4 style={{ marginBottom: 10 }}>Deprecation Warning</h4>
+        <p>The abuse.ch Ransomware Tracker was shut down on 2019-12-08. This Data Adapter should not be used.</p>
       </Alert>
-
     </div>
   );
 };
